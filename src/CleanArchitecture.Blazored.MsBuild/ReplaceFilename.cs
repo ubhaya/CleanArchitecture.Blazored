71c45d8386dde100db9abcedc1a3533110a3d48d
Ubhaya Hettiarachchi <ubhaya123@gmail.com>
2024-01-12 16:37:18 +0530

    Convert Custom MSBuild target to CSharp classes

---
+++ src/CleanArchitecture.Blazored.MsBuild/ReplaceFilename.cs
@@ -0,0 +1,42 @@
+using Microsoft.Build.Framework;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace CleanArchitecture.Blazored.MsBuild;
+
+public class ReplaceFilename : Task
+{
+    [Required] public string Filename { get; set; } = string.Empty;
+    [Required] public string MatchExpression { get; set; } = string.Empty;
+    [Required] public string ReplacementText { get; set; } = string.Empty;
+
+    public override bool Execute()
+    {
+        try
+        {
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(Filename);
+
+            if (!fileNameWithoutExtension.Contains(MatchExpression)) return true;
+            var path = Path.GetDirectoryName(Filename);
+            ArgumentNullException.ThrowIfNull(path);
+            fileNameWithoutExtension = fileNameWithoutExtension.Replace(MatchExpression, ReplacementText);
+            var extension = Path.GetExtension(Filename);
+            var newFileName = Path.Combine(path, $"{fileNameWithoutExtension}{extension}");
+            Log.LogMessage(MessageImportance.High, $"{Filename} -> {newFileName}");
+            try
+            {
+                File.Move(Filename, newFileName, overwrite: true);
+            }
+            catch
+            {
+                File.Delete(Filename);
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Log.LogError(ex.Message);
+            return false;
+        }
+    }
+}
