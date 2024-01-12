c59222f5c223a94ed3224c653f1ac7a0912fc918
Ubhaya Hettiarachchi <ubhaya123@gmail.com>
2024-01-12 15:50:58 +0530

    Add MsBuild Target project

---
+++ src/CleanArchitecture.Blazored.MsBuild/MyTask.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace CleanArchitecture.Blazored.MsBuild;
+
+public class MyTask : Task
+{
+    public string? MyProperty { get; set; }
+    public override bool Execute()
+    {
+        Log.LogMessage(MessageImportance.High, $"The task was passed {MyProperty}.");
+        return true;
+    }
+}
