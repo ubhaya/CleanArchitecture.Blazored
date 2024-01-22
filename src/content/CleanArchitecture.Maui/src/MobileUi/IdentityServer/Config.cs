fcacc1f51823b3a204e990b1f44dda13e18a9198
Ubhaya Hettiarachchi <ubhaya123@gmail.com>
2024-01-22 15:14:36 +0530

    Add basic IdentityServer template

---
+++ src/content/CleanArchitecture.Maui/src/MobileUi/IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new("clean_architecture_maui_api", "CleanArchitecture.Maui.Api"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new()
+            {
+                ClientId = "CleanArchitecture.Maui.MobileUi",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "clean_architecture_maui_api" }
+            }
+        };
+}
