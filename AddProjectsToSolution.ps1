e86f63faf9ac97c875c1210911818f60f22823c9
Ubhaya Hettiarachchi <ubhaya123@gmail.com>
2024-01-26 15:55:52 +0530

    Fix Failing Test

---
+++ AddProjectsToSolution.ps1
@@ -0,0 +1,36 @@
+param (
+    [string]$solutionPath,
+    [string]$projectsPath
+)
+
+# Check if solutionPath and projectsPath are provided
+if (-not $solutionPath -or -not $projectsPath) {
+    Write-Host "Usage: AddProjectsToSolution.ps1 -solutionPath <Path\To\YourSolution.sln> -projectsPath <Path\To\Your\Projects>"
+    exit 1
+}
+
+$isSolutionExists = Test-Path $solutionPath
+
+if (-not $isSolutionExists){
+    $solutionName = [System.IO.Path]::GetFileNameWithoutExtension($solutionPath)
+    $newSolutionCommand = "dotnet new sln -n $solutionName"
+    Invoke-Expression $newSolutionCommand
+    Write-Host "Solution $solutionName is created"
+}
+
+# Get a list of all .csproj files in the projects directory
+$projectFiles = Get-ChildItem -Path $projectsPath -Filter *.csproj -Recurse
+
+# Iterate through each project file and add it to the solution
+foreach ($projectFile in $projectFiles) {
+    # Get the project file name without extension
+     $projectName = [System.IO.Path]::GetFileNameWithoutExtension($projectFile.Name)
+
+    # Add the project to the solution
+    $addProjectCommand = "dotnet sln $solutionPath add $($projectFile.FullName)"
+    Invoke-Expression $addProjectCommand
+
+    Write-Host "Added project '$projectName' to solution."
+}
+
+Write-Host "Build process completed."
