07477b5f98ed362c1d49d43190a02267bb116391
Ubhaya Hettiarachchi <ubhaya123@gmail.com>
2024-02-27 11:09:32 +0530

    Mark todo item done

--- src/content/CleanArchitecture.Maui/src/MobileUi/Mobile/Models/Todo/TodoItemsModel.cs
+++ src/content/CleanArchitecture.Maui/src/MobileUi/Mobile/Models/Todo/TodoItemsModel.cs
@@ -1,13 +1,22 @@
+using CleanArchitecture.Maui.MobileUi.Client;
+using CleanArchitecture.Maui.MobileUi.Shared.TodoItems;
 using CleanArchitecture.Maui.MobileUi.Shared.TodoLists;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CleanArchitecture.Maui.MobileUi.Mobile.Models.Todo;
 
 public sealed partial class TodoItemsModel : ObservableObject
 {
+    private readonly ITodoItemsClient _client;
     [ObservableProperty] private bool _done;
     [ObservableProperty] private string _title = string.Empty;
 
+    private TodoItemsModel(ITodoItemsClient client)
+    {
+        _client = client;
+    }
+    
     public int Id { get; set; }
 
     public int ListId { get; set; }
@@ -16,9 +25,9 @@ public sealed partial class TodoItemsModel : ObservableObject
 
     public string Note { get; set; } = string.Empty;
 
-    public static TodoItemsModel From(TodoItemDto source)
+    public static TodoItemsModel From(TodoItemDto source, ITodoItemsClient client)
     {
-        return new TodoItemsModel
+        return new TodoItemsModel(client)
         {
             Id = source.Id,
             ListId = source.ListId,
@@ -27,5 +36,20 @@ public sealed partial class TodoItemsModel : ObservableObject
             Priority = source.Priority,
             Note = source.Note,
         };
+    }
+
+    [RelayCommand]
+    private async Task CheckChanged()
+    {
+        await _client.PutTodoItemAsync(Id,
+            new UpdateTodoItemRequest()
+            {
+                Id = Id,
+                ListId = ListId,
+                Title = Title,
+                Done = Done,
+                Note = Note,
+                Priority = Priority
+            });
     }
 }
--- src/content/CleanArchitecture.Maui/src/MobileUi/Mobile/ViewModels/Todo/TodoItemsViewModel.cs
+++ src/content/CleanArchitecture.Maui/src/MobileUi/Mobile/ViewModels/Todo/TodoItemsViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using CleanArchitecture.Maui.MobileUi.Client;
 using CleanArchitecture.Maui.MobileUi.Mobile.Models.Todo;
 using CleanArchitecture.Maui.MobileUi.Shared.TodoLists;
 using CommunityToolkit.Maui.Core.Extensions;
@@ -8,16 +9,22 @@ namespace CleanArchitecture.Maui.MobileUi.Mobile.ViewModels.Todo;
 
 public sealed partial class TodoItemsViewModel : BaseViewModel, IQueryAttributable
 {
+    private readonly ITodoItemsClient _itemsClient;
     [ObservableProperty] private string _todoList = string.Empty;
 
     [ObservableProperty] private ObservableCollection<TodoItemsModel> _todoItems = [];
 
+    public TodoItemsViewModel(ITodoItemsClient itemsClient)
+    {
+        _itemsClient = itemsClient;
+    }
+    
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         var todoList = query[nameof(TodoList)] as TodoListDto;
         TodoList = todoList?.Title ?? string.Empty;
         TodoItems = todoList?.Items
-                        .Select(TodoItemsModel.From)
+                        .Select(x=>TodoItemsModel.From(x, _itemsClient))
                         .ToObservableCollection()
                     ?? [];
     }
